f6311c469bbc0d61db882c005d2d02f239c93006
EkaterinaNattrass <cattiva2012@gmail.com>
2025-01-07 15:06:14 +0100

    adding card to the projects section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,52 +1,41 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
-import { Box, Typography, Button, CardActionArea } from "@mui/material";
-import { GitHub, LinkedIn } from "@mui/icons-material";
-import ImageList from "@mui/material/ImageList";
-import ImageListItem from "@mui/material/ImageListItem";
-import Switch from "@mui/material/Switch";
-import Paper from "@mui/material/Paper";
-import Collapse from "@mui/material/Collapse";
-import FormControlLabel from "@mui/material/FormControlLabel";
-
-const itemData = [
-  {
-    img: "images/Holidaze1.png",
-    title: "Home page",
-  },
-  {
-    img: "images/Museum1.png",
-    title: "Books",
-  },
-  {
-    img: "images/Auction1.png",
-    title: "Sink",
-  },
-  {
-    img: "images/Raindays1.png",
-    title: "Kitchen",
-  },
-];
+import {
+  Box,
+  Typography,
+  Button,
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  CardActionArea,
+} from "@mui/material";
+import ArrowForwardRoundedIcon from "@mui/icons-material/ArrowForwardRounded";
 
 export default function Projects() {
   return (
-     <Box sx={{ height: "100vh", backgroundColor: "#f3f3f3" }}>
-      <ImageList sx={{ width: '100%' }} cols={2} rowHeight={200}>
-      {itemData.map((item) => (
-        <CardActionArea key={item.img}>
-                 <ImageListItem key={item.img}>
-          <img
-            srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-            src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
-            alt={item.title}
-            loading="lazy"
-          />
-        </ImageListItem>
-        </CardActionArea>
-   
-      ))}
-    </ImageList>     
-    </Box> 
-
-    )
+    <Box sx={{ height: "100vh", backgroundColor: "#f3f3f3" }}>
+      <Card sx={{ width: "90%", height: "50rem", backgroundColor: '#f8f8f8' }}>
+        <CardMedia
+          sx={{ width: '50%', height: '50rem' }}
+          image="images/Holidaze1.png"
+          title="Holidaze project home page"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">HOLIDAZE</Typography>
+          <Typography variant="body2" sx={{ color: "text.secondary" }}>
+            A holidays accomodation website allowing a customer to book listed
+            venues and a venue owner to manage properties. The website features
+            minimalistic design and colour palette that encourages adventurous
+            mindset and desire to travel. The app is built with MUI.
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Link>
+          <Button>Check out the website <ArrowForwardRoundedIcon /></Button>
+          </Link>
+        </CardActions>
+      </Card>
+    </Box>
+  );
 }
