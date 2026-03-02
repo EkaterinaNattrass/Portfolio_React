c2a54a127874a51d12631cd34fedc9a8cae14d85
EkaterinaNattrass <cattiva2012@gmail.com>
2026-03-02 17:28:51 +0100

    commented out the shop

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -12,7 +12,7 @@ export default function NavigationBar() {
           <NavLink style={{ textDecoration: "none" }} to="/"><Typography sx={{ fontFamily: 'Quicksand', fontWeight: 'bold', color: "#3a2f25", cursor: 'pointer', fontSize: { xs: '0.5rem', sm: '1rem'} }}>EKATERINA // <br /> NATTRASS</Typography></NavLink>
           <NavLink to="frontEnd" style={{ textDecoration: "none" }}><Typography sx={{ fontFamily: 'Quicksand', fontWeight: 'bold', color: '#3a2f25', cursor: 'pointer', fontSize: { xs: '0.5rem', sm: '1rem'} }}>FRONTEND</Typography></NavLink>
           <NavLink to="design" style={{ textDecoration: "none" }}><Typography sx={{ fontFamily: 'Quicksand', fontWeight: 'bold', color: '#3a2f25', cursor: 'pointer', fontSize: { xs: '0.5rem', sm: '1rem'} }}>DESIGN</Typography></NavLink>
-          <NavLink to="shop" style={{ textDecoration: "none" }}><Typography sx={{ fontFamily: 'Quicksand', fontWeight: 'bold', color: '#3a2f25', cursor: 'pointer', fontSize: { xs: '0.5rem', sm: '1rem'} }}>SHOP</Typography></NavLink>
+         {/* <NavLink to="shop" style={{ textDecoration: "none" }}><Typography sx={{ fontFamily: 'Quicksand', fontWeight: 'bold', color: '#3a2f25', cursor: 'pointer', fontSize: { xs: '0.5rem', sm: '1rem'} }}>SHOP</Typography></NavLink> */} 
           <NavLink to="contact" style={{ textDecoration: "none" }}><Typography sx={{ fontFamily: 'Quicksand', fontWeight: 'bold', color: '#3a2f25', cursor: 'pointer', fontSize: { xs: '0.5rem', sm: '1rem'} }}>CONTACTS</Typography></NavLink>
         </Toolbar>
       </AppBar>
