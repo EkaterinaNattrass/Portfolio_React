c24b2a2c5e6676fbbb8adb88c68b254daa41e3b8
EkaterinaNattrass <cattiva2012@gmail.com>
2026-03-03 13:10:22 +0100

    fixed the routing in nav bar

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,9 +1,12 @@
 import * as React from "react";
 import { Box, } from "@mui/material";
+import NavigationBar from "./NavigationBar";
 
 
 export default function Contacts() {
     return (
-        <Box sx={{ height: '100vh'}}></Box>
+        <Box sx={{ height: '100vh'}}>
+            <NavigationBar/>
+        </Box>
     )
 }
