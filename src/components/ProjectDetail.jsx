82d8b92f93cda15bc7371ae53158cd30ac864b7a
EkaterinaNattrass <cattiva2012@gmail.com>
2026-03-05 11:18:36 +0100

    fixed the card layout and height

--- src/components/ProjectDetail.jsx
+++ src/components/ProjectDetail.jsx
@@ -1,4 +1,4 @@
-import { Box, Container, Card, Typography } from "@mui/material";
+import { Box, Card, Typography } from "@mui/material";
 import NavigationBar from "./NavigationBar";
 import { useParams } from "react-router-dom";
 import { projects } from "../data/projects";
@@ -8,27 +8,57 @@ export default function ProjectDetail() {
   const project = projects.find((p) => p.id === Number(id));
   if (!project) return <p>Project not found</p>;
   return (
-    <Box sx={{ position: "relative", height: "100vh", width: "100%" }}>
+    <Box sx={{ width: "100%" }}>
       <NavigationBar />
-      <Container>
-        <Card sx={{ borderRadius: 4, mt: 4, border: "none", boxShadow: "0 4px 20px rgba(0,0,0,0.1)",}}>
-          <Box
-            component="img"
-            src="/images/calm.png"
-            sx={{ width: "100%", mb: 6,  }}
-          />
-          <Typography variant="h3" sx={{ fontFamily: 'work sans'}}>{project.title}</Typography>
-          <Typography variant="h6" sx={{ fontFamily: 'work sans'}}>{project.idea}</Typography>
-          <Typography variant="h6" sx={{ fontFamily: 'work sans'}}>Design decisions</Typography>
+
+      <Card
+        sx={{
+          height: { sm: "auto", md: "90vh" },
+          margin: "5rem",
+          borderRadius: 4,
+          mt: 4,
+          border: "none",
+          boxShadow: "0 4px 20px rgba(0,0,0,0.1)",
+          display: "flex",
+          flexDirection: { sm: "column", md: "row" },
+        }}
+      >
+        <Box component="img" src="/images/calm.png" />
+        <Box
+          sx={{
+            p: 4,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            gap: 2,
+          }}
+        >
+          <Typography variant="h3" sx={{ fontFamily: "work sans" }}>
+            {project.title}
+          </Typography>
+          <Typography variant="h6" sx={{ fontFamily: "work sans" }}>
+            {project.idea}
+          </Typography>
+          <Typography variant="h6" sx={{ fontFamily: "work sans" }}>
+            Design decisions
+          </Typography>
           {project.designDecisions.map((decision, index) => (
-            <Typography key={index} variant="body1" sx={{ fontFamily: 'work sans'}}>
+            <Typography
+              key={index}
+              variant="body1"
+              sx={{ fontFamily: "work sans" }}
+            >
               - {decision}
             </Typography>
           ))}
-          <Typography variant="h6" sx={{ fontFamily: 'work sans'}}>Outcome</Typography>
-          <Typography sx={{ fontFamily: 'work sans'}}>{project.outcome}</Typography>
-        </Card>
-      </Container>
+          <Typography variant="h6" sx={{ fontFamily: "work sans" }}>
+            Outcome
+          </Typography>
+          <Typography sx={{ fontFamily: "work sans" }}>
+            {project.outcome}
+          </Typography>
+        </Box>
+      </Card>
     </Box>
   );
 }
